a325a859a10b865cb5079456b35eeef243fd7be9
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-06-03 13:09:00 +0600

    baseurl change

--- public/baseUrl.js
+++ public/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://doctor-portals-server-4ax5nap0k-md-shuvo-miahs-projects.vercel.app"
-// export const baseUrl = "http://localhost:5000"
+// export const baseUrl = "https://doctor-portals-server-4ax5nap0k-md-shuvo-miahs-projects.vercel.app"
+export const baseUrl = "http://localhost:5000"
