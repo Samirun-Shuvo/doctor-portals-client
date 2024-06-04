21a4a5bfb2986f5c8b5105e068109f3cdef0ddff
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-06-04 15:10:29 +0600

    update env config

--- src/firebase.init.jsx
+++ src/firebase.init.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIiQIxiEIrT9-M5WeUR2cCuobCEP4EO1Y",
-  authDomain: "doctor-portals-dd6eb.firebaseapp.com",
-  projectId: "doctor-portals-dd6eb",
-  storageBucket: "doctor-portals-dd6eb.appspot.com",
-  messagingSenderId: "254959589028",
-  appId: "1:254959589028:web:31f6237656f14f1f4a3831"
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
